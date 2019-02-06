9432a3180f6a1678a03b8309fc20c4293f2d67ce
Jaxon Keeler <jaxon@artifylabs.com>
2019-02-05 17:48:39 -0700

    analyzed fall2 and fall5 videos and recorded observations

--- code/bounding.py
+++ code/bounding.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 ## Get video from data and store in a cv2 videoCapture object
 path = os.path.abspath(os.path.dirname(__file__))
-path = os.path.join(path, "../data/fall1.mp4")
+path = os.path.join(path, "../data/fall5.mp4")
 reel = cv2.VideoCapture(path)
 ## Make background subtractor
 fgbg = cv2.createBackgroundSubtractorMOG2()
@@ -43,8 +43,8 @@ while(curr_frame_i < n_frames):
   print('rect_ratio: {0} | ellipse_angle: {1}'.format(rect_ratios[curr_frame_i], ellipse_angles[curr_frame_i]))
   ## Show the current frame, with any additional things we've drawn superimposed onto the image
   cv2.imshow('result', frame)
-  if(curr_frame_i > 10):
-    cv2.waitKey(0) # This just pauses until you press a key. I'm not sure which keys work, but i know 'n' does
+  # if(curr_frame_i > 10):
+  cv2.waitKey(0) # This just pauses until you press a key. I'm not sure which keys work, but i know 'n' does
   curr_frame_i += 1
 
 reel.release()
@@ -61,10 +61,15 @@ plt.xlabel('Video Frame')
 plt.ylabel('Rectangle Ratio')
 plt.show()
 '''
-Conclusions: Looks like rectangle ratio is indeed a good variable for this. In fall1.mp4, the subjects fall begins
-around frame 15 or so. That's also the point where we begin to see a sharp upward trend in rect_ratio. The subject
+Observations: Looks like rectangle ratio is indeed a good variable for this. 
+--- fall1.mp4 ---
+the subjects fall begins around frame 15 or so. That's also the point where we begin to see a sharp upward trend in rect_ratio. The subject
 is pretty tall and lanky though, which makes him an ideal subject for this type of detection. Wider, shorter subjects
 will see less difference in rect_ratio between standing up and lying down.
+--- fall2.mp4 ---
+When the subject first enters the scene, we pick up her hand and foot as the entire subject. This will cause some weird data points when people enter the view of the camera, so we may want to come up with a way to handle this. We may not need to though with the rest of the pipeline, we'll see
+--- fall5.mp4 ---
+A bit of noise, but not as noisy as the ellipse. Kalman filter should help
 '''
 ## Ellipse angle
 plt.scatter(range(n_frames), ellipse_angles)
@@ -73,7 +78,11 @@ plt.xlabel('Video Frame')
 plt.ylabel('Ellipse Angle w/ Y-axis')
 plt.show()
 '''
-Conclusions: Of course, the angle of the ellipse represents if the person is lying down or not. Values close to 90 
+Observations: Of course, the angle of the ellipse represents if the person is lying down or not. Values close to 90 
 represent lying down (perpendicular to y-axis). The derivative (the third variable we need) is critical to be able 
 to detect a rapid change from standing to lying down
+--- fall2.mp4 ---
+Similar observation to the rectangle ratio for this video.
+--- fall5.mp4 ---
+This was super noisy for some reason! This is a good video to test the effect of the kalman filter, because it should help a lot with this noise
 '''

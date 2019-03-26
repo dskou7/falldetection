b1c90e4c8260095c2927e4a594242ee946886c5e
Jaxon Keeler <jaxon@artifylabs.com>
2019-03-26 16:04:26 -0600

    WIP KNN classifier

---
+++ code/classifier.py
@@ -0,0 +1,23 @@
+'''
+Step 1: Loads data set from our csv file into a pandas df
+Step 2: Partitions into training and test sets
+Step 3: "Trains" KNN, and then predicts for test.
+Step 4: Important metrics gathered on KNN classifier (accuracy for now. Will want precision, recall, and probably others)
+'''
+
+import cv2
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+### Step 1: Loads data set from our csv file into a pandas df
+
+
+### Step 2: Partitions into training and test sets
+
+
+### Step 3: "Trains" KNN, and then predicts for test.
+
+
+### Step 4: Important metrics gathered on KNN classifier
